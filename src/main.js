191a7444899ca3a811673bb53d3fd8b013cd9d51
joelgrod <joelgrod@gmail.com>
2021-08-30 17:27:42 +0100

    vuex and domains

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 // styles
 import './css/styles.scss'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
