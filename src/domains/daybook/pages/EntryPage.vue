77929dadb18f055d249457ac8d56f83c44633861
joelgrod <joelgrod@gmail.com>
2021-09-01 11:17:44 +0100

    EntryPage min fixes

--- src/domains/daybook/pages/EntryPage.vue
+++ src/domains/daybook/pages/EntryPage.vue
@@ -1,36 +1,42 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <!-- TODO -->
-      <span class="text-success fs-3 fw-bold">{{ getDay }}</span>
-      <span class="mx-1 fs-3">{{ getMonth }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ getYearDay }}</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ getDay }}</span>
+        <span class="mx-1 fs-3">{{ getMonth }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ getYearDay }}</span>
+      </div>
+
+      <div>
+        <button class="btn btn-danger mx-2">
+          Delete
+          <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary">
+          Upload Image
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
 
-    <div>
-      <button class="btn btn-danger mx-2">
-        Delete
-        <i class="fa fa-trash-alt"></i>
-      </button>
-      <button class="btn btn-primary">
-        Upload Image
-        <i class="fa fa-upload"></i>
-      </button>
+    <hr />
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea
+        class="entry-textarea"
+        placeholder="What's happen?"
+        v-model="entry.text"
+      ></textarea>
     </div>
-  </div>
 
-  <hr />
+    <img
+      src="https://images.pexels.com/photos/4245826/pexels-photo-4245826.jpeg?cs=srgb&dl=pexels-riccardo-bertolo-4245826.jpg&fm=jpg"
+      alt="entry-photo"
+      class="img-thumbnail entry-img"
+    />
+  </template>
+
   <FabComponent icon="fa-save"></FabComponent>
-
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea class="entry-textarea" placeholder="What's happen?" v-model="entry.text"></textarea>
-  </div>
-
-  <img
-    src="https://images.pexels.com/photos/4245826/pexels-photo-4245826.jpeg?cs=srgb&dl=pexels-riccardo-bertolo-4245826.jpg&fm=jpg"
-    alt="entry-photo"
-    class="img-thumbnail entry-img"
-  />
+  
 </template>
 
 <script>
@@ -39,44 +45,45 @@ import { mapGetters } from "vuex";
 import getDayMonthYear from "../helpers/getDayMonthYear";
 
 export default {
-  name: 'entry-page',
+  name: "entry-page",
   props: {
     entryId: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   data() {
     return {
-      entry: null
-    }
+      entry: null,
+    };
   },
   methods: {
     loadEntry() {
-      const entry = this.getEntryById( this.entryId )
-      if( entry === undefined ) return this.$router.push({ name: 'Daybook-No-Entry' })
-      this.entry = entry
-    }
+      const entry = this.getEntryById(this.entryId);
+      if (entry === undefined)
+        return this.$router.push({ name: "Daybook-No-Entry" });
+      this.entry = entry;
+    },
   },
   computed: {
-    ...mapGetters('daybook', ['getEntryById']),
+    ...mapGetters("daybook", ["getEntryById"]),
     getDay() {
-      const { day } = getDayMonthYear(this.entry.date)
-      return day
+      const { day } = getDayMonthYear(this.entry.date);
+      return day;
     },
     getMonth() {
-      const { month } = getDayMonthYear(this.entry.date)
-      return month
+      const { month } = getDayMonthYear(this.entry.date);
+      return month;
     },
     getYearDay() {
-      const { yearDay } = getDayMonthYear(this.entry.date)
-      return yearDay
+      const { yearDay } = getDayMonthYear(this.entry.date);
+      return yearDay;
     },
   },
   watch: {
     entryId() {
-      this.loadEntry()
-    }
+      this.loadEntry();
+    },
   },
   components: {
     FabComponent: defineAsyncComponent(() =>
@@ -84,8 +91,8 @@ export default {
     ),
   },
   created() {
-    this.loadEntry()
-  }
+    this.loadEntry();
+  },
 };
 </script>
 
@@ -101,10 +108,10 @@ export default {
 }
 
 .entry-img {
-    position: fixed;
-    bottom: 9rem;
-    right: 1rem;
-    width: 12rem;
-    box-shadow: 0px 5px 10px rgba($color: #333333, $alpha: 0.2);
+  position: fixed;
+  bottom: 9rem;
+  right: 1rem;
+  width: 12rem;
+  box-shadow: 0px 5px 10px rgba($color: #333333, $alpha: 0.2);
 }
 </style>
