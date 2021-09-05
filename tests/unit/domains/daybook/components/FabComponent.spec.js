fecfe316706581a16e9bdaf5a2d7fe58697571e6
JoelGRod <joelgrod@gmail.com>
2021-09-06 00:19:21 +0100

    FabComponent tests

--- tests/unit/domains/daybook/components/FabComponent.spec.js
+++ tests/unit/domains/daybook/components/FabComponent.spec.js
@@ -13,14 +13,34 @@ describe('Testing FabComponent.vue', () => {
     });
 
     test('should show the default icon (fa-plus)', () => {
-        
+        // Arrange
+        const icon = wrapper.find('i');
+        // Act
+        // Assert
+        expect( icon.classes('fa-plus') ).toBeTruthy();    
     });
 
     test('should show the fa-circle icon', () => {
-        
+        // Arrange
+        const wrapper = shallowMount( FabComponent, {
+            props: {
+                icon: 'fa-circle'
+            }
+        });
+        const icon = wrapper.find('i');
+        // Act
+        // Assert
+        expect( icon.classes('fa-circle') ).toBeTruthy();
     });
 
-    test('should emit on:click event when click', () => {
-        
+    test('should emit on:click event when click', async () => {
+        // Arrange
+        const button = wrapper.find('button');
+        // Act
+        button.trigger('click');
+        console.log(wrapper.emitted());
+        // Assert
+        expect( wrapper.emitted('on:click') ).toBeTruthy();
+        expect( wrapper.emitted('on:click') ).toHaveLength(1);  
     });
 })
