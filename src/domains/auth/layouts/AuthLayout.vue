c8448fa283c4d5c2af4f1a71d0d5727017e2878e
joelgrod <joelgrod@gmail.com>
2021-09-19 14:35:06 +0100

    auth module pages

--- src/domains/auth/layouts/AuthLayout.vue
+++ src/domains/auth/layouts/AuthLayout.vue
@@ -3,7 +3,9 @@
 
 		<div class="container-login100" style="background-image: url('./assets/bg-01.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
+
 				<router-view></router-view>
+				
 			</div>
 		</div>
 	</div>
--- src/domains/auth/pages/LoginPage.vue
+++ src/domains/auth/pages/LoginPage.vue
@@ -1,16 +1,17 @@
 <template>
   <span class="login100-form-title p-b-41"> Ingresar </span>
   <form class="login100-form validate-form p-b-33 p-t-5">
-    <div class="wrap-input100 validate-input" data-validate="Enter username">
-      <input class="input100" type="text" placeholder="Correo" required />
-      <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+
+    <div class="wrap-input100 validate-input" data-validate="Enter email">
+      <input class="input100" type="text" placeholder="Email" required />
+      <span class="focus-input100" data-placeholder="&#xe818;"></span>
     </div>
 
     <div class="wrap-input100 validate-input" data-validate="Enter password">
       <input
         class="input100"
         type="password"
-        placeholder="Contraseña"
+        placeholder="Password"
         required
       />
       <span class="focus-input100" data-placeholder="&#xe80f;"></span>
@@ -21,7 +22,7 @@
     </div>
 
     <div class="container-login100-form-btn m-t-32">
-      <router-link :to="{}">¿No tienes cuenta?</router-link>
+      <router-link :to="{ name: 'Register' }">You do not have an account?</router-link>
     </div>
   </form>
 </template>
@@ -29,6 +30,3 @@
 <script>
 export default {};
 </script>
-
-<style>
-</style>
