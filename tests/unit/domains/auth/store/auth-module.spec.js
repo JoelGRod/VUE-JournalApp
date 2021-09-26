1567eec9d5c1b997e621779a6223da343e8ffed6
joelgrod <joelgrod@gmail.com>
2021-09-26 10:38:12 +0100

    testing vuex - auth store - create user action

--- tests/unit/domains/auth/store/auth-module.spec.js
+++ tests/unit/domains/auth/store/auth-module.spec.js
@@ -1,7 +1,6 @@
 import createVuexStore from "../../../mock-data/mock-store";
 
 describe("Vuex - Testing auth store module", () => {
-
   /* ---------------- State ---------------- */
   test("should have this initial state", () => {
     // Arrange
@@ -112,5 +111,44 @@ describe("Vuex - Testing auth store module", () => {
   });
 
   /* ---------------- Actions ---------------- */
-  
+  test("Testing actions: createUser - User Already Exists", async () => {
+    // Arrange
+    const store = createVuexStore({
+      status: "not-authenticated", // 'authenticated', 'not-authenticated'. 'authenticating'
+      user: null,
+      idToken: null,
+      refreshToken: null,
+    });
+
+    const newUser = { email: "testing@test.com", password: "123456" };
+    // Act
+    const { msg } = await store.dispatch("auth/createUser", newUser);
+    const { status, user, idToken, refreshToken } = store.state.auth;
+    // Assert
+    expect(msg).toBe("EMAIL_EXISTS");
+
+    expect(status).toBe("not-authenticated");
+    expect(user).toBeNull();
+    expect(idToken).toBeNull();
+    expect(refreshToken).toBeNull();
+  });
+
+  test('Testing actions: createUser', async() => {
+    // Arrange
+    const store = createVuexStore({
+      status: "not-authenticated", // 'authenticated', 'not-authenticated'. 'authenticating'
+      user: null,
+      idToken: null,
+      refreshToken: null,
+    });
+
+    const newUser = { name: 'test user' ,email: "testing2@test.com", password: "123456" };
+    // Act
+    // Delete user
+    await store.dispatch('auth/loginUser', newUser);
+    const { idToken } = store.state.auth;
+    console.log(idToken);
+    // Use authApi for this TODO!!
+    
+  });
 });
