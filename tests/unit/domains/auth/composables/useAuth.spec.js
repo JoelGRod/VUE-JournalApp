24fbc340dad93ba4ef34b0d1a4da601bdb2d257c
joelgrod <joelgrod@gmail.com>
2021-09-27 18:03:52 +0100

    Vuex testing: useAuth composable

--- tests/unit/domains/auth/composables/useAuth.spec.js
+++ tests/unit/domains/auth/composables/useAuth.spec.js
@@ -1,133 +1,148 @@
 import useAuth from "@/domains/auth/composables/useAuth";
 
 const mockStore = {
-    dispatch: jest.fn(),
-    // commit
-    // getters
-}
+  dispatch: jest.fn(),
+  commit: jest.fn(),
+  getters: {
+      'auth/currentStatus': 'authenticated',
+      'auth/username': 'test'
+  },
+};
 
-jest.mock('vuex', () => ({
-    useStore: () => mockStore
-}))
+jest.mock("vuex", () => ({
+  useStore: () => mockStore,
+}));
 
-describe('Testing useAuth Composable', () => {
+describe("Testing useAuth Composable", () => {
+  beforeEach(() => jest.clearAllMocks());
 
-    beforeEach( () => jest.clearAllMocks() );
+  test("createUser successful", async () => {
+    // Arrange
+    const { createUser } = useAuth();
 
-    test('createUser successful', async() => {
-        // Arrange
-        const { createUser } = useAuth();
+    const newUser = {
+      name: "test",
+      email: "test@test.com",
+      password: "123456",
+    };
 
-        const newUser = {
-            name: 'test',
-            email: 'test@test.com',
-            password: '123456'
-        };
+    mockStore.dispatch.mockReturnValue({ ok: true });
 
-        mockStore.dispatch.mockReturnValue( { ok: true } );
+    // Act
+    const resp = await createUser(newUser);
 
-        // Act
-        const resp = await createUser( newUser );
+    // Assert
+    expect(mockStore.dispatch)
+        .toHaveBeenCalledWith("auth/createUser", newUser);
+    expect(resp).toMatchObject({ ok: true });
+  });
 
-        // Assert
-        expect(mockStore.dispatch)
-            .toHaveBeenCalledWith('auth/createUser', newUser);
-        expect( resp ).toMatchObject( { ok: true } )
-    });
+  test("createUser unsuccessful - email exists", async () => {
+    // Arrange
+    const { createUser } = useAuth();
 
-    test('createUser unsuccessful - email exists', async() => {
-        // Arrange
-        const { createUser } = useAuth();
+    const newUser = {
+      name: "test",
+      email: "test@test.com",
+      password: "123456",
+    };
 
-        const newUser = {
-            name: 'test',
-            email: 'test@test.com',
-            password: '123456'
-        };
+    mockStore.dispatch
+        .mockReturnValue({ ok: false, msg: "EMAIL_EXISTS" });
 
-        mockStore.dispatch.mockReturnValue(
-            { ok: false, msg: 'EMAIL_EXISTS' }
-        );
+    // Act
+    const resp = await createUser(newUser);
 
-        // Act
-        const resp = await createUser( newUser );
+    // Assert
+    expect(mockStore.dispatch)
+        .toHaveBeenCalledWith("auth/createUser", newUser);
+    expect(resp)
+        .toEqual({ ok: false, msg: "EMAIL_EXISTS" });
+  });
 
-        // Assert
-        expect( mockStore.dispatch )
-            .toHaveBeenCalledWith('auth/createUser', newUser);
-        expect( resp ).toEqual( { ok: false, msg: 'EMAIL_EXISTS' } );
-    });
+  test("loginUser successful", async () => {
+    // Arrange
+    const { loginUser } = useAuth();
 
-    test('loginUser successful', async() => {
-        // Arrange
-        const { loginUser } = useAuth();
+    const user = {
+      email: "test@test.com",
+      password: "123456",
+    };
 
-        const user = {
-            email: 'test@test.com',
-            password: '123456'
-        };
+    mockStore.dispatch.mockReturnValue({ ok: true });
 
-        mockStore.dispatch.mockReturnValue( { ok: true } );
+    // Act
+    const resp = await loginUser(user);
 
-        // Act
-        const resp = await loginUser( user );
+    // Assert
+    expect(mockStore.dispatch)
+        .toHaveBeenCalledWith("auth/loginUser", user);
+    expect(resp).toEqual({ ok: true });
+  });
 
-        // Assert
-        expect( mockStore.dispatch )
-            .toHaveBeenCalledWith('auth/loginUser', user);
-        expect( resp ).toEqual( { ok: true } );
-    });
+  test("loginUser unsuccessful", async () => {
+    // Arrange
+    const { loginUser } = useAuth();
 
-    test('loginUser unsuccessful', async() => {
-        // Arrange
-        const { loginUser } = useAuth();
+    const user = {
+      email: "test@bad.com",
+      password: "123456",
+    };
 
-        const user = {
-            email: 'test@bad.com',
-            password: '123456'
-        };
+    mockStore.dispatch
+        .mockReturnValue({ ok: false, msg: "INVALID_USER" });
 
-        mockStore.dispatch.mockReturnValue( 
-            { ok: false, msg: 'INVALID_USER' } 
-        );
+    // Act
+    const resp = await loginUser(user);
 
-        // Act
-        const resp = await loginUser( user );
+    // Assert
+    expect(mockStore.dispatch)
+        .toHaveBeenCalledWith("auth/loginUser", user);
+    expect(resp)
+        .toEqual({ ok: false, msg: "INVALID_USER" });
+  });
 
-        // Assert
-        expect( mockStore.dispatch )
-            .toHaveBeenCalledWith('auth/loginUser', user);
-        expect( resp ).toEqual( 
-            { ok: false, msg: 'INVALID_USER' } 
-        );
-    });
+  test("checkAuthStatus successful", async () => {
+    // Arrange
+    const { checkAuthStatus } = useAuth();
+    mockStore.dispatch.mockReturnValue({ ok: true });
+    // Act
+    const resp = await checkAuthStatus();
+    // Assert
+    expect(mockStore.dispatch)
+        .toHaveBeenCalledWith("auth/checkAuth");
+    expect(resp).toEqual({ ok: true });
+  });
 
-    test('checkAuthStatus successful', async() => {
-        // Arrange
-        const { checkAuthStatus } = useAuth();
-        mockStore.dispatch.mockReturnValue( { ok: true } );
-        // Act
-        const resp = await checkAuthStatus();
-        // Assert
-        expect( mockStore.dispatch )
-            .toHaveBeenCalledWith('auth/checkAuth');
-        expect(resp).toEqual( { ok: true } );
-    });
+  test("checkAuthStatus unsuccessful", async () => {
+    // Arrange
+    const { checkAuthStatus } = useAuth();
+    mockStore.dispatch
+        .mockReturnValue({ ok: false, msg: "WRONG_ID_TOKEN" });
+    // Act
+    const resp = await checkAuthStatus();
+    // Assert
+    expect(mockStore.dispatch)
+        .toHaveBeenCalledWith("auth/checkAuth");
+    expect(resp)
+        .toEqual({ ok: false, msg: "WRONG_ID_TOKEN" });
+  });
 
-    test('checkAuthStatus unsuccessful', async() => {
-        // Arrange
-        const { checkAuthStatus } = useAuth();
-        mockStore.dispatch.mockReturnValue( 
-            { ok: false, msg: 'WRONG_ID_TOKEN' } 
-        );
-        // Act
-        const resp = await checkAuthStatus();
-        // Assert
-        expect( mockStore.dispatch )
-            .toHaveBeenCalledWith('auth/checkAuth');
-        expect(resp).toEqual( 
-            { ok: false, msg: 'WRONG_ID_TOKEN' } 
-        );
-    });
+  test("logout", () => {
+    // Arrange
+    const { logout } = useAuth();
+    // Act
+    logout();
+    // Assert
+    expect(mockStore.commit)
+        .toHaveBeenCalledWith("auth/logout");
+  });
 
+  test("computed: getAuthStatus and getUsername", () => {
+    // Arrange
+    const { getAuthStatus, getUsername } = useAuth();
+    // Assert
+    expect( getAuthStatus.value ).toBe('authenticated');
+    expect( getUsername.value ).toBe('test');
+  });
 });
